03abace5c4657206bc4a020d92464494c634f561
zineblouniri <zineblouniri@gmail.com>
2026-06-16 15:39:22 +0100

    User authentication (register/login + JWT + bcrypt)

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import pool from "./config/db.js";
+import authRoute from "./routes/authRoute.js"
 
 const app = express();
 
@@ -9,6 +10,8 @@ dotenv.config();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth" ,authRoute)
 
 app.get("/", (req, res) => {
   res.json({ message: "API is running" });
